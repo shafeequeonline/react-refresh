f2a6144e0fcacf399c10dda7ea5af1f53a1c82c9
shafeeque <shafeeque@archis.company>
2022-10-23 19:10:08 +0200

    Worked with base templates, styles, list, keys, Props, states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import pokemon from "./pokemon.json";
+import PropTypes from "prop-types";
+import React from "react";
+
+const PokemonRow = ({ pokemon, onSelect }) => (
+  <tr key={pokemon.id}>
+    <td>{pokemon.name.english} </td>
+    <td>{pokemon.type.join(", ")} </td>
+    <td>
+      <button onClick={() => onSelect(pokemon)}>Select!</button>
+    </td>
+  </tr>
+);
+
+const PokemonInfo = ({ name, base }) => (
+  <div className="card">
+    <h2>{name.english}</h2>
+    <table>
+      <tbody>
+        {Object.keys(base).map((key) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
+
+PokemonRow.prototype = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+    onSelect: PropTypes.func,
+  }),
+};
+
+PokemonInfo.prototype = {
+  name: PropTypes.shape({
+    english: PropTypes.string.isRequired,
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number,
+    "Sp. Attack": PropTypes.number,
+    "Sp. Defense": PropTypes.number,
+    Speed: PropTypes.number,
+  }),
+};
 
 function App() {
+  const [filter, setFilter] = React.useState("");
+  const [selectedItem, setSelectedItem] = React.useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: 800,
+        margin: "auto",
+        paddingTop: "1rem",
+      }}
+    >
+      <h1 className="title">Pokemon Search</h1>
+      <input
+        type="text"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <div>
+        <div>
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                .filter((pokemon) =>
+                  pokemon.name.english.toLowerCase().includes(filter)
+                )
+                .slice(0, 20)
+                .map((pokemon) => (
+                  <PokemonRow
+                    pokemon={pokemon}
+                    key={pokemon.id}
+                    onSelect={(pokemon) => setSelectedItem(pokemon)}
+                  ></PokemonRow>
+                ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && (
+          <div>
+            <PokemonInfo {...selectedItem}></PokemonInfo>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
